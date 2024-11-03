1543aae38f5330d8ccd899f99e64a6f9ced7129d
Caio Monte <caiomm076@gmail.com>
2024-11-03 16:21:32 -0300

    página inicial

--- site/FirebaseConfig.js
+++ site/FirebaseConfig.js
@@ -21,24 +21,24 @@ const analytics = getAnalytics(app);
 const db = getFirestore(app);
 
 
-async function listarProdutos() {
-  const Ebooks = await getDocs(collection(db, "E-book"));
+// Função para listar produtos
+async function listarProdutos() {                               
+
+  const Ebooks = await getDocs(collection(db, "E-book"));           // Exibição dos E-books
   const listaEBook = Ebooks.docs.map(doc => doc.data());
-  
-  const containerProdutos = document.getElementById("produtos-ebook");
+  var containerProdutos = document.getElementById("produtos-ebook");
 
   listaEBook.forEach(produto => {
     const produtoDiv = document.createElement("div");
     produtoDiv.innerHTML = `
       <div class="m-2 col-10">
               <div class="card product-card shadow-sm">
-                  <img src="https://via.placeholder.com/300x200" class="card-img-top" alt="E-book 1">
+                  <img src="${produto.foto}" class="card-img-top" alt="E-book 1">
                   <div class="card-body">
                       <h5 class="card-title">${produto.nome}</h5>
                       <p class="card-text">${produto.descricao}</p>
-                      <p class="text-primary fw-bold">R$ ${produto.preco}</p>
+                      <p class="text-primary fw-bold">R$ ${produto.preco.toFixed(2)}</p>
                       <a href="#" class="btn btn-primary">Adicionar ao Carrinho</a>
-                      <a href="#" class="btn btn-outline-primary">Detalhes</a>
                   </div>
               </div>
       </div>
@@ -47,10 +47,59 @@ async function listarProdutos() {
   });
 
 
-  const Livros = await getDocs(collection(db, "E-book"));
-  const listaLivros = Livros.docs.map(doc => doc.data());
 
-  
+
+  const Apostila = await getDocs(collection(db, "Apostila"));            // Exibição das Apostilas
+  const listaApostila = Apostila.docs.map(doc => doc.data());
+  const ApostilaContainer = document.getElementById("produtos-apostila");
+
+  listaApostila.forEach(produto => {
+    const produtoDiv = document.createElement("div");
+    produtoDiv.innerHTML = `
+      <div class="m-2 col-10">
+              <div class="card product-card shadow-sm">
+                  <img src="${produto.foto}" class="card-img-top" alt="E-book 1">
+                  <div class="card-body">
+                      <h5 class="card-title">${produto.nome}</h5>
+                      <p class="card-text">${produto.descricao}</p>
+                      <p class="text-primary fw-bold">R$ ${produto.preco.toFixed(2)}</p>
+                      <a href="#" class="btn btn-primary">Adicionar ao Carrinho</a>
+                  </div>
+              </div>
+      </div>
+      `;
+      ApostilaContainer.appendChild(produtoDiv);
+  });
+
+
+
+
+  const Curso = await getDocs(collection(db, "Curso"));            // Exibição dos Cursos
+  const ListaCurso = Curso.docs.map(doc => doc.data());
+  const CursoContainer = document.getElementById("produtos-curso");
+
+  ListaCurso.forEach(produto => {
+    const produtoDiv = document.createElement("div");
+    produtoDiv.innerHTML = `
+      <div class="m-2 col-10">
+              <div class="card product-card shadow-sm">
+                  <img src="${produto.foto}" class="card-img-top card-imagem" alt="E-book">
+                  <div class="card-body">
+                      <h5 class="card-title">${produto.nome}</h5>
+                      <p class="card-text">${produto.descricao}</p>
+                      <p class="text-primary fw-bold">R$ ${produto.preco.toFixed(2)}</p>
+                      <a href="#" class="btn btn-primary">Adicionar ao Carrinho</a>
+                  </div>
+              </div>
+      </div>
+      `;
+      CursoContainer.appendChild(produtoDiv);
+  });
+
+
+
+
+
 
 }
 
